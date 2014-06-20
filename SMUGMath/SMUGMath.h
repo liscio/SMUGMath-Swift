9cab624706e99a3dd52d5d98409f0e26ed17e9a0
Christopher Liscio <chris@Christophers-MacBook-Pro.local>
2014-06-20 16:32:34 -0400

    Initial Commit

---
+++ SMUGMath/SMUGMath.h
@@ -0,0 +1,19 @@
+//
+//  SMUGMath.h
+//  SMUGMath
+//
+//  Created by Christopher Liscio on 2014-06-20.
+//  Copyright (c) 2014 Christopher Liscio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SMUGMath.
+FOUNDATION_EXPORT double SMUGMathVersionNumber;
+
+//! Project version string for SMUGMath.
+FOUNDATION_EXPORT const unsigned char SMUGMathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SMUGMath/PublicHeader.h>
+
+
